741dd45ceedc4bf425331877cba90d2293820db9
chunhelv <chunhelv@pateo.com.cn>
2023-09-19 13:52:27 +0800

    chunhelv 修改dockfile文件

--- src/main/java/com/lvch/scaffold/common/controller/ExportReportController.java
+++ src/main/java/com/lvch/scaffold/common/controller/ExportReportController.java
@@ -1,5 +1,6 @@
 package com.lvch.scaffold.common.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @apiNote
  */
 @RestController
-//@Slf4j
+@Slf4j
 public class ExportReportController {
 
 
     @GetMapping("/test")
     public String appExportReport() {
+        log.error("xxxxxxxxxxx");
+
         return "success!";
     }
 
     @GetMapping("/testtttt")
     public String test() {
+        log.info("ccccccccccc");
+
         return "===============success!============";
     }
 
