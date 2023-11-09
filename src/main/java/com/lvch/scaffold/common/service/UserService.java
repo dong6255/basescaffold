3c1912aa7ac5ff0073623e8575e493287c06551a
April <chunhelv@foxmail.com>
2023-11-09 23:59:41 +0800

    April 新增用户详情接口

--- src/main/java/com/lvch/scaffold/common/service/UserService.java
+++ src/main/java/com/lvch/scaffold/common/service/UserService.java
@@ -3,6 +3,7 @@ package com.lvch.scaffold.common.service;
 
 
 import com.lvch.scaffold.common.domain.entity.User;
+import com.lvch.scaffold.common.domain.vo.response.user.UserInfoResp;
 
 import java.util.List;
 
@@ -15,6 +16,14 @@ import java.util.List;
  * @since 2023-03-19
  */
 public interface UserService {
+
+    /**
+     * 获取前端展示信息
+     *
+     * @param uid
+     * @return
+     */
+    UserInfoResp getUserInfo(Long uid);
 
     /**
      * 用户注册，需要获得id
